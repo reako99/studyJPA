147840cbcb449f31783e07a7e6f08c0143af1272
reako99 <zldrgx@naver.com>
2024-05-27 22:47:19 +0900

    Create Project studyJPA

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,10 @@
+package hellojpa;
+
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        Persistence.createEntityManagerFactory("hello");
+    }
+}
