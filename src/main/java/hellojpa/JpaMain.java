9a4db9a14fe01b3ce6ed5ca82f9fea7ec7567f47
reako99 <zldrgx@naver.com>
2024-05-28 00:12:26 +0900

    Update JpaMain.java

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,20 +9,22 @@ public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-
-        member.setId(1L);
-        member.setName("helloA");
-
-        em.persist(member);
-
-        tx.commit();
-
-        em.close();
+        try{
+            Member findMember = em.find(Member.class,1L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+            findMember.setName("HelloJPA");
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
