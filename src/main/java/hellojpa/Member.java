044a39c44a59da953ccf2c27de64efaa39ff2b81
reako99 <zldrgx@naver.com>
2024-06-16 23:05:08 +0900

    Update Member.java

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,26 +5,34 @@ import java.util.Date;
 
 @Entity
 @Table(name = "member")
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1,
+        allocationSize = 1
+)
 public class Member {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
+    private Long id;
     @Column(name = "name", nullable = false)
     private String username;
 
     public Member() {
     }
 
-    public Member(String id, String username) {
+    public Member(Long id, String username) {
         this.id = id;
         this.username = username;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
